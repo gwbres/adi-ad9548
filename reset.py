f76f878ca94fbdc29350b69ef0a5a07abcd5904a
gwbres <guillaume.bressaix@gmail.com>
2022-05-04 14:15:17 +0200

    remove irq + non needed flag

--- reset.py
+++ reset.py
@@ -31,7 +31,6 @@ def main (argv):
     )
     flags = [
         ('soft', 'Performs a soft reset but maintains current registers value'),
-        ('irq',  'Resets all IRQs'),
         ('lf',   'Clears digital loop filter'),
         ('cci',  'Clears cci filter'),
         ('phase','Clears dds phase accumulator'),
@@ -54,36 +53,25 @@ def main (argv):
 
     if args.soft:
         reg = read_data(handle, address, 0x0A00)
-        reg &= 0x7E # clear bits
         write_data(handle, address, 0x0000, reg | 0x01 | 0x80)
         write_data(handle, address, 0x0000, reg)
-    if args.irq:
-        reg = read_data(handle, address, 0x0A03)
-        reg &= 0xFD
-        write_data(handle, address, 0x0A03, reg | 0x02)
     if args.watchdog:
         reg = read_data(handle, address, 0x0A03)
-        reg &= 0xFE
         write_data(handle, address, 0x0A03, reg | 0x01)
     if args.lf:
         reg = read_data(handle, address, 0x0A03)
-        reg &= 0xBF
         write_data(handle, address, 0x0A03, reg | 0x40)
     if args.cci:
         reg = read_data(handle, address, 0x0A03)
-        reg &= 0xDF
         write_data(handle, address, 0x0A03, reg | 0x20)
     if args.phase:
         reg = read_data(handle, address, 0x0A03)
-        reg &= 0xEF
         write_data(handle, address, 0x0A03, reg | 0x10)
     if args.autosync:
         reg = read_data(handle, address, 0x0A03)
-        reg &= 0xF7
         write_data(handle, address, 0x0A03, reg | 0x08)
     if args.history:
         reg = read_data(handle, address, 0x0A03)
-        reg &= 0xFB
         write_data(handle, address, 0x0A03, reg | 0x04)
 if __name__ == "__main__":
     main(sys.argv[1:])
