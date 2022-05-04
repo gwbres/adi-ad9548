6adff8ae1ea2b9a487fb9f430f17ef8927c2a510
gwbres <guillaume.bressaix@gmail.com>
2022-05-04 10:48:34 +0200

    working on profile script

--- profile.py
+++ profile.py
@@ -53,7 +53,7 @@ def gamma (g0, g1):
 def quantize_delta (delta):
     x = -math.ceil(math.log2(delta))
     d1 = min(31, max(0, x))
-    y = round(delta * pow(2,15+b1))
+    y = round(delta * pow(2,15+d1))
     d0 = min(32767, max(1,y))
     return (d0,d1)
 
@@ -92,21 +92,32 @@ def main (argv):
         help='Read internal profile content',
     )
     flags = [
-        ("freq", float, "Read/Set reference frequency [Hz] for given profile"),
-        ("inner", float, "Read/Set inner tolerance [ppm]"),
-        ("outter", float, "Read/Set, outter tolerance [ppm]"),
-        ("validation", float, "validation timer [s]"),
-        ("redetect", float, "redetect timer [s]"),
-        ("alpha", float, "Filter alpha coefficient"),
-        ("beta", float, "Filter beta coefficient"),
-        ("delta", float, "Filter delta coefficient"),
-        ("gamma", float, "Filter gamma coefficient"),
+        ("scaling", str, ['nano','pico'], "Control the phase lock threshold scaling"),
+        ("freq", float, [], "Read/set reference frequency [Hz] for given profile"),
+        ("inner", float, [], "Read/set inner tolerance [ppm]"),
+        ("outter", float, [], "Read/set outter tolerance [ppm]"),
+        ("validation", float, [], "Read/set validation timer [s]"),
+        ("redetect", float, [], "Read/set redetect timer [s]"),
+        ("alpha", float, [],  "Read/set Filter alpha coefficient"),
+        ("beta", float,  [],  "Read/set Filter beta coefficient"),
+        ("delta", float, [],  "Read/set Filter delta coefficient"),
+        ("gamma", float, [],  "Read/set Filter gamma coefficient"),
     ]
-    for (v_flag, v_type, v_helper) in flags:
-        parser.add_argument(
-            "--{}".format(v_flag),
-            type=v_type,
-            help=v_helper)
+
+    for (v_flag, v_type, v_choices, v_helper) in flags:
+        if v_type is None:
+            parser.add_argument(
+                "--{}".format(v_flag),
+                type=v_type,
+                choices=v_choices,
+                help=v_helper)
+        else:
+            parser.add_argument(
+                "--{}".format(v_flag),
+                type=v_type,
+                choices=v_choices,
+                help=v_helper)
+    
     args = parser.parse_args(argv)
 
     # open device
@@ -114,59 +125,82 @@ def main (argv):
     handle.open(int(args.bus))
     address = int(args.address, 16)
 
-    reg0 = 0x0601 
-    size = 0x0631 - reg0 
+    reg0 = 0x0600 
+    size = 0x0632 - reg0
+
+    scalings = {
+        0: 10E-12,
+        1: 10E-9,
+    }
 
     if args.read:
         base = reg0 + size * args.read
         print("debug: base_address is {}".format(hex(base)))
         profile = {}
-        per  = read_data(handle, address, base +0)
-        per += read_data(handle, address, base +1) <<8
-        per += read_data(handle, address, base +2) <<16
-        per += read_data(handle, address, base +3) <<24
-        per += read_data(handle, address, base +4) <<32
-        per += read_data(handle, address, base +5) <<40
-        per += (read_data(handle, address, base +6) & 0x3) <<48
-        profile['freq'] = 1.0/(per * 1E-15)
-        inner  = read_data(handle, address, base +7)
-        inner += read_data(handle, address, base +8) << 8
-        inner += (read_data(handle, address, base +9) & 0x0F) << 8
-        profile['inner'] = inner
-        inner  = read_data(handle, address, base +10)
-        inner += read_data(handle, address, base +11) << 8
-        inner += (read_data(handle, address, base +12) & 0x0F) << 8
-        profile['outter'] = inner
-        timer  = read_data(handle, address, base +13)
-        timer += read_data(handle, address, base +14) << 8
-        profile['validation'] = timer *1E-3
-        timer  = read_data(handle, address, base +15)
-        timer += read_data(handle, address, base +16) << 8
-        profile['redetect'] = timer *1E-3
-        a0  = read_data(handle, address, base +17)
-        a0 += read_data(handle, address, base +18) << 8
-        a  = read_data(handle, address, base +19)
-        a1 = a & 0x1F
-        a2 = (a & 0xC0)>>6
-        b  = read_data(handle, address, base +20)
-        a2 += (b & 0x01) << 2
-        b0 = (b & 0xFE)>>1
-        b0 += read_data(handle, address, base +21) << 8
-        r = read_data(handle, address, base +22)
+
+        r = read_data(handle, address, base + 0x0600-reg0)
+        profile['selection-priority'] = r & 0x07
+        profile['promoted-priority'] = (r & 0x38)>>3
+        scaling = scalings[(r & 0x80)>>7]
+        
+        per  = read_data(handle, address, base + 0x601-reg0)
+        per += read_data(handle, address, base + 0x602-reg0) <<8
+        per += read_data(handle, address, base + 0x603-reg0) <<16
+        per += read_data(handle, address, base + 0x604-reg0) <<24
+        per += read_data(handle, address, base + 0x605-reg0) <<32
+        per += read_data(handle, address, base + 0x606-reg0) <<40
+        per += (read_data(handle, address, base + 0x607-reg0) & 0x3) <<48
+        profile['freq'] = 1.0/(per * pow(10,-15)) # fs
+
+        profile['tolerance'] = {}
+        v  = read_data(handle, address, base  + 0x608-reg0)
+        v += read_data(handle, address, base  + 0x609-reg0) << 8
+        v += (read_data(handle, address, base + 0x60A-reg0) & 0x0F) << 16
+        profile['tolerance']['inner'] = v 
+
+        v  = read_data(handle, address, base  + 0x60B-reg0)
+        v += read_data(handle, address, base  + 0x60B-reg0) << 8
+        v += (read_data(handle, address, base + 0x60C-reg0) & 0x0F) << 16
+        profile['tolerance']['outter'] = v
+
+        v = read_data(handle, address, base  + 0x60E-reg0)
+        v += read_data(handle, address, base + 0x60F-reg0) << 8
+        profile['validation'] = v *1E-3
+
+        v = read_data(handle, address, base  + 0x610-reg0)
+        v += read_data(handle, address, base + 0x611-reg0) << 8
+        profile['redetect'] = v *1E-3
+
+        a0  = read_data(handle, address, base + 0x612-reg0)
+        a0 += read_data(handle, address, base + 0x613-reg0)<< 8
+        r  = read_data(handle, address, base  + 0x614-reg0)
+        a1 = r & 0x1F
+        a2 = (r & 0xC0)>>6
+        
+        r  = read_data(handle, address, base + 0x615-reg0)
+        a2 += (r & 0x01) << 2
+        b0 = (r & 0xFE)>>1
+
+        b0 += read_data(handle, address, base + 0x616-reg0) << 8
+        r = read_data(handle, address, base + 0x617-reg0)
         b0 += (r & 0x03) << 16
         b1 = (r & 0x7C)>>2
-        g0 = read_data(handle, address, base +23)
-        g0 += read_data(handle, address, base +24) << 8
-        r = read_data(handle, address, base +25)
+        
+        g0 = read_data(handle, address, base + 0x618-reg0)
+        g0 += read_data(handle, address, base + 0x619-reg0) << 8
+        r = read_data(handle, address, base + 0x61A - reg0)
         g0 += (r&0x01) << 16
-        g1 = (r&0x3E) >> 1
-        d0 = read_data(handle, address, base+26)
-        r = read_data(handle, address, base+27)
-        d0 += (r&0x7F) << 8
-        d1 = ((r & 0x80) >> 7) << xxxx
-        r = read_data(handle, address, base+28)
+        g1 = (r&0x1E) >> 1
+
+        d0 = read_data(handle, address, base + 0x61B - reg0)
+        r = read_data(handle, addres, base + 0x61C - reg0)
+        d0 += (r&0x7F)<<8 
+        d1 = (r&0x80)>>7
+        
+        r = read_data(handle, address, base + 0x61D - reg0)
         d1 += (r & 0x0F) << 1
         a3 = (r & 0xF0)>>4
+
         profile['alpha'] = alpha(a0,a1,a2,a3)
         profile['beta'] = beta(b0,b1)
         profile['delta'] = delta(d0,d1)
@@ -195,22 +229,65 @@ def main (argv):
         profile['fractionnal-div']['U'] = U
         profile['fractionnal-div']['V'] = V
 
+        profile['lock'] = {}
+        profile['lock']['phase'] = {}
+        v = read_data(handle, address, base + 0x0629-reg0)
+        v+= read_data(handle, address, base + 0x062A-reg0) <<8
+        profile['lock']['phase']['threshold'] = v * scaling
+        profile['lock']['phase']['fill'] = read_data(handle, address, base + 0x062B-reg0)
+        profile['lock']['phase']['drain'] = read_data(handle, address, base + 0x062C-reg0)
+        profile['lock']['freq'] = {}
+        v = read_data(handle, address, base + 0x062D-reg0)
+        v+= read_data(handle, address, base + 0x062E-reg0) <<8
+        v+= read_data(handle, address, base + 0x062F-reg0) <<16
+        profile['lock']['freq']['threshold'] = v *pow(10,-6) #ps
+        profile['lock']['freq']['fill'] = read_data(handle, address, base + 0x0630-reg0)
+        profile['lock']['freq']['drain'] = read_data(handle, address, base + 0x0631-reg0)
+
         print(json.dumps(profile, sort_keys=True, indent=2))
         return 0
 
     profile = args.load
     base = reg0 + size * args.load
     print("debug: base_address is {}".format(hex(base)))
-    
+
+    if args.scaling:
+        scalings = {
+            'nano': 1,
+            'pico': 0,
+        }
+        r = read_data(handle, address, base + 0x0600-reg0)
+        r |= scalings[args.scaling] << 7
+        write_data(handle, address, base + 0x0600-reg0, r)
+   
+    if args.promotion_priority:
+        r = read_data(handle, address, base + 0x0600-reg0)
+        r |= (args.promotion_priority & 0x07) << 3
+        write_data(handle, address, base + 0x0600-reg0, r)
+
+    if args.selection_priority:
+        r = read_data(handle, address, base + 0x0600-reg0)
+        r |= (args.selection_priority & 0x07)
+        write_data(handle, address, base + 0x0600-reg0, r)
+
     if args.freq:
         per = round(1E15/args.freq)
-        write_data(handle, address, base +0, per & 0xFF)
-        write_data(handle, address, base +1, (per & 0xFF00)>>8)
-        write_data(handle, address, base +2, (per & 0xFF0000)>>16)
-        write_data(handle, address, base +3, (per & 0xFF000000)>>24)
-        write_data(handle, address, base +4, (per & 0xFF00000000)>>32)
-        write_data(handle, address, base +5, (per & 0xFF0000000000)>>40)
-        write_data(handle, address, base +7, (per & 0xFF000000000000)>>48)
+        write_data(handle, address, base + 0x0601-reg0, per & 0xFF)
+        write_data(handle, address, base + 0x0602-reg0, (per & 0xFF00)>>8)
+        write_data(handle, address, base + 0x0603-reg0, (per & 0xFF0000)>>16)
+        write_data(handle, address, base + 0x0604-reg0, (per & 0xFF000000)>>24)
+        write_data(handle, address, base + 0x0605-reg0, (per & 0xFF00000000)>>32)
+        write_data(handle, address, base + 0x0606-reg0, (per & 0xFF0000000000)>>40)
+        write_data(handle, address, base + 0x0607-reg0, (per & 0x03000000000000)>>48)
+
+    if args.inner:
+        write_data(handle, address, base + 0x0608-reg0, args.inner & 0xFF)
+        write_data(handle, address, base + 0x0609-reg0, (args.inner & 0xFF00)>>8)
+        write_data(handle, address, base + 0x060A-reg0, (args.inner & 0xF00000)>>16)
+    if args.outter:
+        write_data(handle, address, base + 0x060B-reg0, args.outter  & 0xFF)
+        write_data(handle, address, base + 0x060C-reg0, (args.outter & 0xFF00)>>8)
+        write_data(handle, address, base + 0x060A-reg0, (args.outter & 0xF00000)>>16)
 
     if args.alpha:
         q = quantize(args.alpha, 'alpha')
